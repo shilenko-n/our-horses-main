6263bc1b1c7b957a1951d05e97189badf05a50cf
Nikolay Shilenko <shilenko.n@studio2i.ru>
2025-08-25 12:09:30 +0300

    Подготовка модели User, City и Country. Создание контроллеров и миграций

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -19,8 +20,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
+        'balance',
+        'age',
+        'city',
     ];
 
     /**
