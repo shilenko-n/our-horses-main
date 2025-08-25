35c7b086cdba5b09e9047face6152d9c07c74f11
Nikolay Shilenko <shilenko.n@studio2i.ru>
2025-08-25 13:03:22 +0300

    Подготовка моделей уведомлений, тем для технических вопрос и технические вопросы

---
+++ database/migrations/2025_08_25_100118_create_technical_support_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('technical_support_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('technical_support_request_topic_id')->constrained('technical_support_request_topics');
+            $table->text('content');
+            $table->boolean('closed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('technical_support_requests');
+    }
+};
