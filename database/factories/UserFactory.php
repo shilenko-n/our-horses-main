aef654c078e55f4ee22483e938ee519100c87f23
Nikolay Shilenko <shilenko.n@studio2i.ru>
2025-08-27 14:40:11 +0300

    Добавление User seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\City;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,11 +25,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->firstName(),
+            'surname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'age' => fake()->numberBetween(18, 50),
+            'city_id' => City::query()->inRandomOrder()->first()->id,
         ];
     }
 
